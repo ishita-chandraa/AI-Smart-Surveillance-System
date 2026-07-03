b38fbb18cec7e4361e145ed614ed1b24fcd3c636
Ishita Chandra <ishitachandra0508@gmail.com>
2026-07-03 22:08:22 +0530

    Add dynamic detection history to dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from database import (
+    get_total_detections,
+    get_today_count,
+    get_weekly_count,
+    get_monthly_count,
+    get_latest_detection,
+    get_all_detections
+)
 
 app = FastAPI()
 
@@ -9,8 +17,17 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def home(request: Request):
 
+    context = {
+    "total": get_total_detections(),
+    "today": get_today_count(),
+    "week": get_weekly_count(),
+    "month": get_monthly_count(),
+    "latest": get_latest_detection(),
+    "detections": get_all_detections()
+}
+
     return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={}
+    request=request,
+    name="index.html",
+    context=context
     )
